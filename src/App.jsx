10dbe539b9237f5fef13ca813ff596e16d4d2662
@sahakyan <saakyan@visionsystems.tech>
2024-11-24 00:46:16 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.module.css';
+import AppHeader from "./components/app-header/AppHeader";
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/app-header/AppHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './app-header.module.css';
+
+
+function AppHeader() {
+  return (
+      <header className={styles.header}>
+
+      </header>
+  );
+}
+
+export default AppHeader;
